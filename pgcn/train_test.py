4d1fd579cc8c6cb229d7317b9b71c37f1f5533d8
azimG <azimgivron@gmail.com>
2025-06-23 18:11:51 +0200

    Update train_test.py

--- pgcn/train_test.py
+++ pgcn/train_test.py
@@ -59,7 +59,7 @@ def train(
 
     num_pos = data["gene", "assoc", "disease"].edge_index.size(1)
 
-    # Log hyperparameters (including dropout rate)
+    # Log hyperparameter
     writer.add_hparams(
         {
             "lr": lr,
